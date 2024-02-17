0afc862688233f6cd8cfd904824c02387830781f
VitaliiShevchenko <iksel@ukr.net>
2024-02-17 10:35:47 +0200

    Create Controller and Router

---
+++ controllers/user.controller.js
@@ -0,0 +1,23 @@
+class UserController {
+    async createUser(req, res) {
+
+    }
+
+    async getUsers(req, user) {
+
+    }
+
+    async getUser(req, user) {
+
+    }
+
+    async updateUser(req, user) {
+
+    }
+
+    async deleteUser(req, user) {
+
+    }
+}
+
+module.exports = new UserController()
